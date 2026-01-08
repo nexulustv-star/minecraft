1fdf945fefcc8d543d6aa04648655b7cb0a03323
nexulustv-star <nexulustv@gmail.com>
2026-01-09 03:15:58 +1000

    Update smile.lua

--- smile.lua
+++ smile.lua
@@ -1,44 +1,85 @@
--- smiley_monitor.lua
-local mon = peripheral.find("monitor")
+-- startup.lua - Runs when turtle boots
+print("=== Standalone Turtle Booting ===")
+print("ID: " .. os.getComputerID())
+print("Time: " .. os.date())
+print("")
 
-if not mon then
-    print("No monitor connected!")
-    print("Place a monitor next to the computer")
-    return
+-- Set label if not set
+if not os.getComputerLabel() then
+    os.setComputerLabel("AutoTurtle-" .. os.getComputerID())
 end
 
--- Set up monitor
-mon.setTextScale(2)  -- Make text bigger
-mon.setBackgroundColor(colors.black)
-mon.setTextColor(colors.yellow)
-mon.clear()
+-- Check fuel
+local fuel = turtle.getFuelLevel()
+print("Fuel: " .. fuel)
+if fuel < 500 then
+    print("⚠ Warning: Low fuel!")
+    print("Place fuel in inventory and run: turtle.refuel()")
+end
 
--- Get monitor size
-local width, height = mon.getSize()
+-- Check for updates every 24 hours
+local lastUpdateFile = "data/last_update.txt"
+local updateInterval = 86400  -- 24 hours in seconds
 
--- Calculate center position
-local centerX = math.floor(width / 2)
-local centerY = math.floor(height / 2)
+if fs.exists(lastUpdateFile) then
+    local file = fs.open(lastUpdateFile, "r")
+    local lastUpdate = tonumber(file.readLine()) or 0
+    file.close()
+    
+    local currentTime = os.time()
+    if currentTime - lastUpdate > updateInterval then
+        print("Checking for GitHub updates...")
+        shell.run("github_manager.lua", "1")
+        
+        -- Update timestamp
+        local file = fs.open(lastUpdateFile, "w")
+        file.writeLine(tostring(currentTime))
+        file.close()
+    end
+else
+    -- First time setup
+    print("First boot - downloading scripts...")
+    shell.run("github_manager.lua", "1")
+    
+    local file = fs.open(lastUpdateFile, "w")
+    file.writeLine(tostring(os.time()))
+    file.close()
+end
 
--- Draw smiley face in center
-mon.setCursorPos(centerX - 4, centerY - 3)
-mon.write("  *******  ")
-mon.setCursorPos(centerX - 4, centerY - 2)
-mon.write(" *       * ")
-mon.setCursorPos(centerX - 4, centerY - 1)
-mon.write("*  O   O  *")
-mon.setCursorPos(centerX - 4, centerY)
-mon.write("*         *")
-mon.setCursorPos(centerX - 4, centerY + 1)
-mon.write("*   ___   *")
-mon.setCursorPos(centerX - 4, centerY + 2)
-mon.write(" * \\___/ * ")
-mon.setCursorPos(centerX - 4, centerY + 3)
-mon.write("  *******  ")
+-- Main menu
+sleep(2)
+term.clear()
+term.setCursorPos(1, 1)
 
--- Add text below
-mon.setTextColor(colors.white)
-mon.setCursorPos(centerX - 5, centerY + 5)
-mon.write("Have a nice day! :)")
+print("=== Standalone Turtle Control ===")
+print("1. Start Player Follower")
+print("2. Start Patrol Mode")
+print("3. Check GitHub Updates")
+print("4. Fuel Status")
+print("5. Run Mining Program")
+print("6. Shutdown")
+print("")
 
-print("Smiley displayed on monitor!")
+while true do
+    write("Choice: ")
+    local choice = read()
+    
+    if choice == "1" then
+        shell.run("scripts/player_follower.lua")
+    elseif choice == "2" then
+        shell.run("scripts/patrol_follower.lua")
+    elseif choice == "3" then
+        shell.run("github_manager.lua", "1")
+    elseif choice == "4" then
+        print("Fuel: " .. turtle.getFuelLevel())
+        print("Max: " .. turtle.getFuelLimit())
+    elseif choice == "5" then
+        shell.run("scripts/miner.lua")
+    elseif choice == "6" then
+        print("Shutting down...")
+        os.sleep(2)
+        os.shutdown()
+    end
+    
+    print("")
+end
